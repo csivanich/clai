6c7f73e8b47c3922403fd6568241d1eb8aff8104
Chris Sivanich <csivanich@anyscale.com>
2023-08-25 11:05:28 -0700

    fix: THIS_DIR not traversing symlinks
    https://stackoverflow.com/a/246128

--- endpoints.sh
+++ endpoints.sh
@@ -10,7 +10,13 @@ if ! which pv &>/dev/null;then
     }
 fi
 
-THIS_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+THIS_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
 # TODO: make this relative to the script's local directory
 MODELS_DIR="${THIS_DIR}/models"
