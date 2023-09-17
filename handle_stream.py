fe9819ffc60c26d76bd9db6c0d51d31975f555be
Chris Sivanich <csivanich@anyscale.com>
2023-09-16 19:51:01 -0700

    feat: generic error handling

--- handle_stream.py
+++ handle_stream.py
@@ -1,28 +1,50 @@
 #!/usr/bin/env python3
 
 import sys
-import io
+from io import TextIOWrapper
 import json
 
-# Create an unbuffered input stream
-input_stream = io.TextIOWrapper(
-    sys.stdin.buffer, encoding=sys.stdin.encoding, errors=sys.stdin.errors
-)
 
-# Process lines from the unbuffered input stream
-while True:
-    line = input_stream.readline().rstrip()
+class StreamError(Exception):
+    pass
 
-    if not line:
-        break
 
-    parts = line.split(" ")
+def on_error(code, message):
+    raise StreamError(f"({code}) {message}")
 
-    if parts[1] == "[DONE]":
-        break
 
-    for c in json.loads(" ".join(parts[1:])).get("choices", []):
-        if "content" in c.get("delta", {}):
-            print(c["delta"]["content"], end="", flush=True)
+def handle(input):
+    while True:
+        line = input.readline().rstrip()
 
-print(flush=True)
+        # There's empty lines between the JSON stanzas in some implementations
+        if line == "":
+            continue
+
+        # But if it's false/None, we're done
+        if not line:
+            break
+
+        # TODO: don't spit and rejoin, just parse until the space
+        parts = line.split(" ")
+
+        if parts[0] == "data:":
+            if parts[1] == "[DONE]":
+                break
+
+            parsed_data = json.loads(" ".join(parts[1:]))
+
+            e = parsed_data.get("error", {})
+            if e:
+                code = e.get("code", -1)
+                message = e.get("message", "Unknown error occurred")
+                raise StreamError(code, message)
+
+            for c in parsed_data.get("choices", []):
+                if "content" in c.get("delta", {}):
+                    print(c["delta"]["content"], end="", flush=True)
+
+
+if __name__ == "__main__":
+    handle(sys.stdin)
+    print(flush=True)
